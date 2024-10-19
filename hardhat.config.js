7575270e0765aef35fca397c592866647cb807a8
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2024-10-19 10:10:29 -0300

    added networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+
 require("dotenv").config();
 const provider = process.env.PROVIDER;
 const providerMainnet = process.env.PROVIDER_MAINNET;
+const providerBase = process.env.PROVIDER_BASE;
+const providerBaseSepolia = process.env.PROVIDER_BASE_SEPOLIA;
+
 const privateKey = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: {
@@ -22,6 +25,14 @@ module.exports = {
 		},
 		mainnet: {
 			url: providerMainnet,
+			accounts: [privateKey],
+		},
+		base: {
+			url: providerBase,
+			accounts: [privateKey],
+		},
+		baseSepolia: {
+			url: providerBaseSepolia,
 			accounts: [privateKey],
 		},
 	},
