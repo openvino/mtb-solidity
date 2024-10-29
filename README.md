69fce551b72412bde338ef972fbcef3ea792408d
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2024-10-29 13:38:53 -0300

    base

--- README.md
+++ README.md
@@ -2,30 +2,15 @@
 
 Before deploying, first deploy minter.
 
-1. Add .env file and add the desired provider and private key and minter
+1.  Add .env file and private key
+2.  Check ./utils/tokens for adjusting token's names, symbols and supply (for this script cap = supply, we will mint all available tokens to the deployer. The values are equal to the current supply of mainnet tokens
+    )
+3.  Deploy and mint all tokens:
 
 ```
-PRIVATE_KEY=
-# Base Sepolia
-PROVIDER_BASE_SEPOLIA=
+       npm i
+       npx hardhat compile
+       npx hardhat run "scripts/deploy.js" --network base
 
-# Base
-PROVIDER_BASE=
 
-# OP SEPOLIA
-PROVIDER=
-
-# Mainnet
-PROVIDER_MAINNET=
-
-TOKEN_MINTER=
-
-```
-
-2.
-
-```shell
-npm i
-npx hardhat compile
-npx hardhat run "scripts/deployWithMinter.js" --network targetNetwork
 ```
--- contracts/mtb.sol
+++ contracts/mtb.sol
@@ -3,12 +3,11 @@ pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MTB is ERC20, ERC20Burnable, ERC20Pausable, ERC20Capped, AccessControl {
-    bytes32 internal constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract MTB is ERC20, ERC20Burnable, ERC20Capped, ERC20Pausable, Ownable {
 
     constructor(
         string memory name, 
@@ -19,36 +18,20 @@ contract MTB is ERC20, ERC20Burnable, ERC20Pausable, ERC20Capped, AccessControl 
         ERC20Capped(cap)
         public
     {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(MINTER_ROLE, msg.sender); 
+        _mint(msg.sender, cap); 
     }
 
-    // Función para obtener todos los minters
-    function allMinters() public view returns (address[] memory) {
-        uint256 count = getRoleMemberCount(MINTER_ROLE);
-        address[] memory minters = new address[](count);
-        for (uint256 i = 0; i < count; i++) {
-            minters[i] = getRoleMember(MINTER_ROLE, i);
-        }
-        return minters;
+   
+    function pause() public onlyOwner {
+        _pause();
     }
 
-    function mint(address to, uint256 amount) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
-        _mint(to, amount);
+    
+    function unpause() public onlyOwner {
+        _unpause();
     }
 
-    function addMinter(address account) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        grantRole(MINTER_ROLE, account);
-    }
-
-    function removeMinter(address account) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        revokeRole(MINTER_ROLE, account);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable, ERC20Capped) {
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Capped, ERC20Pausable) {
         super._beforeTokenTransfer(from, to, amount); 
     }
 
