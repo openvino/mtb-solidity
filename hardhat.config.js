b036bc12fe60f1e2b1bf90f451da03a3d3f1089b
Pablo Levy <pabloelevy@gmail.com>
2025-04-22 20:07:11 -0300

    mtb

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
-
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
+
 const provider = process.env.PROVIDER;
 const providerMainnet = process.env.PROVIDER_MAINNET;
 const providerBase = process.env.PROVIDER_BASE;
 const providerBaseSepolia = process.env.PROVIDER_BASE_SEPOLIA;
-
 const privateKey = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: {
-    version: "0.6.12",
+    version: "0.8.22",
     settings: {
       optimizer: {
         enabled: true,
@@ -19,21 +19,29 @@ module.exports = {
     }
   },
   networks: {
-		targetNetwork: {
-			url: provider,
-			accounts: [privateKey],
-		},
-		mainnet: {
-			url: providerMainnet,
-			accounts: [privateKey],
-		},
-		base: {
-			url: providerBase,
-			accounts: [privateKey],
-		},
-		baseSepolia: {
-			url: providerBaseSepolia,
-			accounts: [privateKey],
-		},
-	},
+    targetNetwork: {
+      url: provider,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: providerMainnet,
+      accounts: [privateKey],
+    },
+    base: {
+      url: providerBase,
+      accounts: [privateKey],
+    },
+    baseSepolia: {
+      url: providerBaseSepolia,
+      accounts: [privateKey],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      base: process.env.BASESCAN_API_KEY,
+      baseSepolia: process.env.BASESCAN_API_KEY,
+    },
+  },
 };
--- utils/tokens.js
+++ utils/tokens.js
@@ -1,10 +1,10 @@
 const tokens = [
-    {  name: "MIKETANGOBRAVO18", symbol: "MTB18", cap: 13887 },
-    {  name: "MIKETANGOBRAVO19", symbol: "MTB19", cap: 17586 },
-    { name: "MIKETANGOBRAVO20", symbol: "MTB20", cap: 9552 },
-    {  name: "MIKETANGOBRAVO21", symbol: "MTB21", cap: 11702 },
-    {  name: "MIKETANGOBRAVO22", symbol: "MTB22", cap: 8192 },
-    {  name: "MIKETANGOBRAVO23", symbol: "MTB23", cap: 1024 }
+    {  name: "FALOPA2", symbol: "FLP2", cap: 13887 },
+    // {  name: "MIKETANGOBRAVO19", symbol: "MTB19", cap: 17586 },
+    // { name: "MIKETANGOBRAVO20", symbol: "MTB20", cap: 9552 },
+    // {  name: "MIKETANGOBRAVO21", symbol: "MTB21", cap: 11702 },
+    // {  name: "MIKETANGOBRAVO22", symbol: "MTB22", cap: 8192 },
+    // {  name: "MIKETANGOBRAVO23", symbol: "MTB23", cap: 1024 }
 ];
 
 module.exports = { tokens };
